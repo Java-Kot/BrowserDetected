ffa4b41fbe903b9034033b26abf74ff486c3cf75
Kot <kotvnn@msn.com>
2018-10-19 13:05:44 +0700

    Done

---
+++ src/main/java/practice/codegym/BrowserDetectedServlet.java
@@ -0,0 +1,21 @@
+package practice.codegym;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BrowserDetectedServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String browser = request.getHeader("User-Agent");
+
+        PrintWriter writer = response.getWriter();
+        writer.println(browser);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: conifer
+  Date: 19/10/2018
+  Time: 12:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+    <meta http-equiv="refresh" content="1;url=/detected" />
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
